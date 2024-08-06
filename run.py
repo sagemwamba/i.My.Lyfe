e2a0e8855c528f33effa17eb6bcd5884f4547631
sage-mwamba <sagesebastian66@.com>
2024-08-06 15:41:47 +0300

    Update Procfile to point to run.py and verify app object

--- run.py
+++ run.py
@@ -4,9 +4,14 @@
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
+from app import app  # Import the Flask app object from the app module
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+
